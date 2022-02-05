c3a0dc67a5bc231859cf6c2ef981e074f7b66767
harukoya <ka.uni.stu@gmail.com>
2022-02-05 18:37:42 +0900

    add login. add event

--- config/application.rb
+++ config/application.rb
@@ -19,10 +19,12 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module App
+module AwesomeEvents
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    config.time_zone = "Tokyo"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
